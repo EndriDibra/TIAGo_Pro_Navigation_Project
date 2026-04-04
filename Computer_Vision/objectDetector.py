1deb2fecbd784d52e8b182d4e4f715dcfefecdbc
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2026-04-04 12:43:44 +0200

    Add files via upload

---
+++ Computer_Vision/objectDetector.py
@@ -0,0 +1,36 @@
+import cv2 
+from ultralytics import YOLO
+
+
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+model = YOLO("yolo11n-pose.pt")
+
+if not camera:
+
+    print("Error! Camera did not open.")
+
+    exit()
+
+while camera.isOpened():
+
+    success, frame = camera.read()
+
+    if not success:
+
+        print("Error! Camera stopped.")
+
+        break
+
+    yolo_frame = model(frame, save=False)
+
+    yolo_frame = yolo_frame[0].plot()
+
+    cv2.imshow("Video Display", yolo_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+
+        break
+
+camera.release()
+cv2.destroyAllWindows() 
