6b20665720f9ecd1a39514500703d655fa4f65ba
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2026-04-04 12:54:03 +0200

    Refactor comments in objectDetector.py for clarity
    Updated comments for clarity and improved readability.

--- Computer_Vision/objectDetector.py
+++ Computer_Vision/objectDetector.py
@@ -2,36 +2,50 @@
 import cv2 
 from ultralytics import YOLO
 
-
+# Using the default camera of the computer system
 camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+# Loading the YOLOv11n model for
+# object detection and recognition
 model = YOLO("yolo11n-pose.pt")
 
+# Checking if the camera is working
 if not camera:
 
     print("Error! Camera did not open.")
 
     exit()
 
+# Looping through camera frames for online streaming
 while camera.isOpened():
 
+    # Reading each camera frame
     success, frame = camera.read()
 
+    # Checking if the camera works
+    # during the online streaming
     if not success:
 
         print("Error! Camera stopped.")
 
         break
 
-    yolo_frame = model(frame, save=False)
+    # Applying YOLOv11n on each camera frame
+    # for online object detection and recognition
+    yolo_Frame = model(frame, save=False)
 
-    yolo_frame = yolo_frame[0].plot()
+    # Plotting around each detected
+    # object the bounding boxes 
+    yolo_Frame = yolo_Frame[0].plot()
 
-    cv2.imshow("Video Display", yolo_frame)
+    # Displaying the video with the process
+    cv2.imshow("Video Display", yolo_Frame)
 
+    # Terminating the process by pressing letter "q"
     if cv2.waitKey(1) & 0xFF == ord("q"):
 
         break
 
+# Releasing camera and windows resources
 camera.release()
 cv2.destroyAllWindows() 
